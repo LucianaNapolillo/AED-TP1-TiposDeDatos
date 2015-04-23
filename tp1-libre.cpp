95c4a86a9545f0a2f85b6add756e872b2563dbc0
Luciana <luciana@luciana-100NZC.(none)>
2015-04-23 00:21:11 -0300

    Agrego tipo de dato char, elimino validaciones no tan necesarias para el ejercicio

--- tp1-libre.cpp
+++ tp1-libre.cpp
@@ -9,56 +9,35 @@ using namespace std;
 int main() {
 
 	string c = "\u2666 ", nombre, apellido, mayorNombreCompleto;
-	int cantidadPersonas, contadorPersonas=0, dia, mes, anio, diaActual, mesActual, anioActual, edad, edades=0, mayor=0;
+	int cantidadPersonas=0, dia, mes, anio, diaActual, mesActual, anioActual, edad, edades=0, mayor=0;
 	bool cumplioAnios;	
 	double promedio;
-
-	bool diaValido(int dia);
-	bool mesValido(int mes);
-	bool anioValido(int anio, int anioActual);
-	bool fechaValida(int dia, int mes, int anio);
-
-	cout << "¿Cuántas personas desea ingresar?: ";
-	cin >> cantidadPersonas;
+	char respuesta;
 
 	cout << "\nIngrese la fecha actual: ";
-	do {
-		do {
-			cout << "\n\tDia: ";
-			cin >> diaActual;
-		} while(!diaValido(diaActual));
-		do {
-			cout << "\tMes: ";
-			cin >> mesActual;
-		} while(!mesValido(mesActual));
+	cout << "\n\tDia: ";
+	cin >> diaActual;
+	cout << "\tMes: ";
+	cin >> mesActual;
+	cout << "\tAño: ";
+	cin >> anioActual;
 		
-			cout << "\tAño: ";
-			cin >> anioActual;
-	} while (!fechaValida(diaActual,mesActual,anioActual));
-	
 	do {
-		cout << c << "Ingresa el nombre de la persona nro. " << contadorPersonas + 1 << ": ";
+		cout << endl << c << "Ingresa el nombre de la persona nro. " << cantidadPersonas + 1 << ": ";
 		cin >> nombre;
 		cout << c << "Ingresa el apellido: ";
 		cin >> apellido;
-		cout << endl << c << "Ingresa la fecha de nacimiento: ";
-		
+		cout << c << "Ingresa la fecha de nacimiento: ";
+		cout << "\n\tDia: ";
+		cin >> dia;
+		cout << "\tMes: ";
+		cin >> mes;
+
 		do {
-			do {
-				cout << "\n\tDia: ";
-				cin >> dia;
-			} while (!diaValido(dia));
-
-			do {
-				cout << "\tMes: ";
-				cin >> mes;
-			} while (!mesValido(mes));
-			do {
-				cout << "\tAño: ";
-				cin >> anio;
-			} while (!anioValido(anio, anioActual));
-		} while (!fechaValida(dia,mes,anio));
-
+			cout << "\tAño: ";
+			cin >> anio;
+		} while (anio <= 0 || anio >= anioActual);
+		
 		cumplioAnios = (mesActual > mes || (mesActual == mes && diaActual == dia));
 		
 		if (cumplioAnios) {
@@ -74,61 +53,16 @@ int main() {
 			mayorNombreCompleto = nombre + " " + apellido;
 		}
 
-		contadorPersonas++;
-	} while(contadorPersonas < cantidadPersonas);
+		cantidadPersonas++;
+		if (cantidadPersonas >= 2) {	//los cálculos sólo se pueden realizar con al menos dos personas
+			cout << "\n¿Desea ingresar mas personas? S/N: ";
+			cin >> respuesta;
+		}
+	} while(cantidadPersonas < 2 || respuesta == 's' || respuesta == 'S');
 
 	promedio = double(edades) / double(cantidadPersonas);
 
 	cout << endl << endl << c << "El promedio de edades es " << promedio << endl;
 	cout << c << "La persona con mayor edad es " << mayorNombreCompleto << endl;
 
-}
-
-bool diaValido (int dia) {
-	if (dia > 0 && dia <= 31) {
-		return 1;
-	} else {
-		cout << "Ingrese un día válido!\n";
-		return 0;
-	}
-}
-
-bool mesValido (int mes) {
-	if (mes > 0 && mes <= 12) {
-		return 1;
-	} else {
-		cout << "Ingrese un mes válido!\n";
-		return 0;
-	}
-}
-
-bool anioValido (int anio, int anioActual) {
-	if (anio > 0 && anio <= anioActual) {
-		return 1;
-	} else {
-		cout << "Ingrese un año válido!\n";
-		return 0;
-	}
-}
-
-bool fechaValida (int dia, int mes, int anio) {
-	if (mes == 2) {
-		if (anio%4 == 0 && (anio%100 != 0 || anio%400 == 0)) {
-			if (dia <= 29) {
-				return 1; 
-			} else {
-				cout << "Fecha inválida!\n";
-				return 0;
-			}
-		} else {
-			if (dia <= 28) {
-				return 1;
-			} else {
-				cout << "Fecha inválida!\n";
-				return 0;
-			}
-		}
-	} else {
-		return 1;
-	}
 }
